193dc69920328c64b655f15251b6a1f83f4868d9
Magnus <magnus.sagerup@gmail.com>
2023-06-12 13:12:56 +0200

    added debounce for search

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -2,32 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { TextField, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { useDebounce } from 'use-debounce';
 import { setSearchQuery } from '../../Redux/Features/currentGenreIdOrCategory';
-
 // import { useDebounce } from 'use-debounce';
 import useStyles from './styles.js';
 
 const Search = () => {
   const classes = useStyles();
   const [query, setQuery] = useState('');
-
+  const [debouncedText] = useDebounce(query, 400);
   const location = useLocation();
   const dispatch = useDispatch();
-  //   console.log('query', query);
 
-  const handleKeyPress = (e) => {
-    if (e.keyCode === 13) {
-      console.log('pressed?', e.keyCode);
-      dispatch(setSearchQuery(query));
+  useEffect(() => {
+    if (debouncedText) {
+      dispatch(setSearchQuery(debouncedText));
     }
-  };
+  }, [debouncedText, dispatch]);
 
   return (
     <div className={classes.searchContainer}>
       <TextField
-        onKeyUp={handleKeyPress}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         variant="standard"
